7e3335818e4ee3ce461c316fafe834870ed87267
Kamil Rudnicki <kamil.rudnicki@gmail.com>
2020-04-26 21:12:48 +0200

    Initial Commit

---
+++ Memento Mori WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  Memento Mori WatchKit Extension
+//
+//  Created by Kamil Rudnicki on 26/04/2020.
+//  Copyright © 2020 TimeCamp S.A. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
